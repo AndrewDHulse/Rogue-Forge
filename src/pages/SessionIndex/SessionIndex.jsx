8af7d025db62cda4c79217b567ffc8800c85c845
AndrewDHulse <theandrewhulse@gmail.com>
2023-08-25 17:34:46 -0500

    add info to index

--- src/pages/SessionIndex/SessionIndex.jsx
+++ src/pages/SessionIndex/SessionIndex.jsx
@@ -1,19 +1,36 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { getAllUsers } from '../../utilities/users-api'; 
+import { findUserNameById } from '../../utilities/users-service';
+import { useState, useEffect } from 'react'; 
 
-export default function SessionIndex({sessions}){
-    console.log('sessions passed to sessionindex',sessions)
-    return(
+export default function SessionIndex({ sessions }) {
+    console.log('sessions passed to sessionindex', sessions);
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function getInfo() {
+            try {
+                const fetchedUsers = await getAllUsers();
+                setUsers(fetchedUsers);
+            } catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        }
+        getInfo();
+    }, []);
+
+    return (
         <>
             <h1>Sessions Index</h1>
             <ul>
-                {sessions.map(session =>(
+                {sessions.map(session => (
                     <li key={session._id}>
-                        <h2>hi</h2>
-                        <p>{session.system}</p>
+                        <h2>{session.campaignName}</h2>
+                        <p>{findUserNameById(users, session.DM)} is running a {session.system} game</p>
                         <Link to={`/sessions/details/${session._id}`}>View Campaign</Link>
                     </li>
                 ))}
             </ul>
         </>
-    )
+    );
 }
